57b2553ace0393ef7669b712d3c0aeee26a68a73
Dinesh Veera bhargav Akula <dineshveerabhargav@gmail.com>
2025-03-02 08:36:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import { connectDB } from './config/db.js';
 import feedbackRoutes from './Routes/feedback.route.js';
 import hrRoutes from './Routes/hr.route.js';
 import workRoutes from './Routes/workRole.route.js';
-
+import path from 'path';
 
 dotenv.config();
 
@@ -13,23 +13,30 @@ const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(cors());
-app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type,Authorization"
-}))
+app.use(
+    cors({
+        origin: ['http://localhost:5173', 'http://localhost:5174'],
+        methods: 'GET,POST,PUT,DELETE',
+        allowedHeaders: 'Content-Type,Authorization',
+    })
+);
 
-app.use("/interview", feedbackRoutes);
-app.use("/hrpage",hrRoutes);
-app.use("/work",workRoutes);
+app.use('/interview', feedbackRoutes);
+app.use('/hrpage', hrRoutes);
+app.use('/work', workRoutes);
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => {
-    connectDB(); 
-    console.log(`Server started at PORT ${PORT}`);
+    connectDB();
+    console.log(Server started at PORT ${PORT});
 });
